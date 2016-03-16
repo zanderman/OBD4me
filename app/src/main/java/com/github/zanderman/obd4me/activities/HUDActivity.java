9fa5d85f191cf188d75408c9d23ee2bd59132feb
Alexander DeRieux <acd1797@vt.edu>
2016-03-15 22:58:52 -0400

    Add new HUD activity which allows user to interface with an OBD adapter.

---
+++ app/src/main/java/com/github/zanderman/obd4me/activities/HUDActivity.java
@@ -0,0 +1,27 @@
+package com.github.zanderman.obd4me.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.github.zanderman.obd4me.R;
+
+
+
+/**
+ * Class:
+ *      HUDActivity
+ *
+ * Description:
+ *      ...
+ *
+ * Author:
+ *      Alexander DeRieux
+ */
+public class HUDActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hud);
+    }
+}
