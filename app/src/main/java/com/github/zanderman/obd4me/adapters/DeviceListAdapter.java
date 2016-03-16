9fe0832c2ff832b035fa6eb566ece3fc5df160d6
Alexander DeRieux <acd1797@vt.edu>
2016-03-15 22:58:18 -0400

    References new HUD activity.

--- app/src/main/java/com/github/zanderman/obd4me/adapters/DeviceListAdapter.java
+++ app/src/main/java/com/github/zanderman/obd4me/adapters/DeviceListAdapter.java
@@ -11,6 +11,7 @@ import android.widget.Toast;
 
 import com.github.zanderman.obd.classes.OBDAdapter;
 import com.github.zanderman.obd4me.R;
+import com.github.zanderman.obd4me.activities.HUDActivity;
 
 import java.util.ArrayList;
 
