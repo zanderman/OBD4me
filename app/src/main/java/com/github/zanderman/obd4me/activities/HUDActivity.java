b52f73d00a40548dbd0a3c92dc31bc87f5b1c3c4
Alexander DeRieux <acd1797@vt.edu>
2016-03-15 23:21:41 -0400

    Added temporary TextView. Retrieves OBDAdapter object from intent.

--- app/src/main/java/com/github/zanderman/obd4me/activities/HUDActivity.java
+++ app/src/main/java/com/github/zanderman/obd4me/activities/HUDActivity.java
@@ -2,7 +2,9 @@ package com.github.zanderman.obd4me.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
+import com.github.zanderman.obd.classes.OBDAdapter;
 import com.github.zanderman.obd4me.R;
 
 
@@ -19,9 +21,26 @@ import com.github.zanderman.obd4me.R;
  */
 public class HUDActivity extends AppCompatActivity {
 
+    /**
+     * Public members.
+     */
+    OBDAdapter device;
+    TextView tempTextView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hud);
+
+        /**
+         * Initialize UI objects.
+         */
+        tempTextView = (TextView) findViewById(R.id.tempTextView);
+
+        // Get passed OBD adapter object.
+        getIntent().getSerializableExtra("OBDAdapter");
+
+        // print device name.
+        tempTextView.setText(device.name);
     }
 }
