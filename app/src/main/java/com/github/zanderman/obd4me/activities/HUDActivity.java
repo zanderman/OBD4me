33e9933da8ad3c4e9e22c7fd6689b838532292a0
Alexander DeRieux <acd1797@vt.edu>
2016-03-19 20:19:13 -0400

    Added additional button to HUD.

--- app/src/main/java/com/github/zanderman/obd4me/activities/HUDActivity.java
+++ app/src/main/java/com/github/zanderman/obd4me/activities/HUDActivity.java
@@ -3,9 +3,11 @@ package com.github.zanderman.obd4me.activities;
 import android.bluetooth.BluetoothDevice;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.github.zanderman.obd.classes.OBDAdapter;
 import com.github.zanderman.obd4me.R;
@@ -28,8 +30,10 @@ public class HUDActivity extends AppCompatActivity implements View.OnClickListen
      * Public members.
      */
     OBDAdapter device;
-    TextView tempTextView;
+    TextView tempNameTextView;
+    TextView tempAddressTextView;
     Button disconnectButton;
+    Button connectButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,14 +43,17 @@ public class HUDActivity extends AppCompatActivity implements View.OnClickListen
         /**
          * Initialize UI objects.
          */
-        tempTextView = (TextView) findViewById(R.id.tempTextView);
+        tempNameTextView = (TextView) findViewById(R.id.tempNameTextView);
+        tempAddressTextView = (TextView) findViewById(R.id.tempAddressTextView);
         disconnectButton = (Button) findViewById(R.id.disconnectButton);
+        connectButton = (Button) findViewById(R.id.connectButton);
 
         // Create OBDAdapter from the passed BluetoothDevice.
         this.device = new OBDAdapter((BluetoothDevice) getIntent().getParcelableExtra("device"));
 
         // print device name.
-        tempTextView.setText(device.name);
+        tempNameTextView.setText(device.name);
+        tempAddressTextView.setText(device.address);
 
         /**
          * Set 'Disconnect' listener.
@@ -72,6 +79,13 @@ public class HUDActivity extends AppCompatActivity implements View.OnClickListen
                 finish();
 
                 break;
+            case R.id.connectButton:
+                if (this.device.connect())
+                    Log.d("HUD","Connected");
+//                    Toast.makeText(this,"Connected",Toast.LENGTH_SHORT);
+                else
+                    Log.d("HUD","Failed...");
+//                    Toast.makeText(this,"Failed...",Toast.LENGTH_SHORT);
         }
     }
 }
