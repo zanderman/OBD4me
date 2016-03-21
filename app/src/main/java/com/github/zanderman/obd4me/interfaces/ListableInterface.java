723f10199c941df4e154623bf418b73292b7ec12
Alexander DeRieux <acd1797@vt.edu>
2016-03-21 15:34:43 -0400

    New interface to denote classes that interact with lists.

---
+++ app/src/main/java/com/github/zanderman/obd4me/interfaces/ListableInterface.java
@@ -0,0 +1,13 @@
+package com.github.zanderman.obd4me.interfaces;
+
+/**
+ * Created by zanderman on 3/21/16.
+ */
+public interface ListableInterface {
+
+    /**
+     * Methods.
+     */
+    public void listAdd( Object o ); /* Add an object to a list. */
+    public void listRemove( Object o ); /* Remove an object from a list. */
+}
